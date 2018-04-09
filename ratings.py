8192367e4357bbc0218beaca16dfe373d8dd3ca4
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 16:49:09 -0700

    Wrote function to validate user_rating, began writing user_choice function.

--- ratings.py
+++ ratings.py
@@ -14,13 +14,47 @@ def get_file_ratings(filename):
     return file_ratings
 
 
+def get_user_choice():
+    print "Enter 1 to see all ratings\n 2 to add a new restaurant\n 'q' to quit"
+    user_choice = raw_input("> ")
+
+    return user_choice
+
+
+def execute_choice(choice, ratings):
+    """Executes user choice"""
+    if choice == "q":
+        return
+    elif choice == "2":
+        get_user_ratings(ratings)
+
+    sort_ratings(ratings)
+
+
 def get_user_ratings(file_ratings):
     """Adds user restaurant and rating"""
 
-    user_restaurant = raw_input("Please enter a restaurant: ")
-    user_rating = raw_input("Please enter the rating: ")
+    class Error(Exception):
+        """Base class for other exceptions"""
+        pass
 
-    file_ratings[user_restaurant] = user_rating
+    class notInRange(Error):
+        """Raised when user_rating is smaller than 1 or bigger than 5"""
+        pass
+
+    try:
+        user_restaurant = raw_input("Please enter a restaurant: ")
+        user_rating = int(raw_input("Please enter the rating: "))
+
+        if user_rating < 1 or user_rating > 5:
+            raise notInRange
+        file_ratings[user_restaurant] = user_rating
+    except notInRange:
+        print "Rating must be a number between 1 and 5. Please try again."
+        get_user_ratings(file_ratings)
+    except:
+        print "Rating must be a number between 1 and 5. Please try again."
+        get_user_ratings(file_ratings)
 
 
 def sort_ratings(restaurants):
@@ -37,8 +71,9 @@ def show_ratings(filename):
 
     ratings = get_file_ratings(filename)
 
-    get_user_ratings(ratings)
+    user_choice = get_user_choice()
 
-    sort_ratings(ratings)
+    execute_choice(user_choice, ratings)
+
 
 show_ratings("scores.txt")
