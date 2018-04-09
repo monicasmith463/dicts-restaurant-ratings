c05ee70472be6d80a7815a193506f4c82d79a81a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 16:01:09 -0700

    Separated file and user rating functions

--- ratings.py
+++ ratings.py
@@ -1,31 +1,44 @@
 """Restaurant rating lister."""
 
 
-# put your code here
-def sort_ratings(restaurants):
-    """Sorts restaurant ratings by restaurant name."""
-
-    sorted_restaurants = sorted(restaurants.keys())
-
-    for restaurant in sorted_restaurants:
-        print "{} is rated at {}.".format(restaurant, restaurants[restaurant])
-
-
-def get_ratings(filename):
+def get_file_ratings(filename):
     """Adds restaurant and rating from user to ratings from file."""
 
-    ratings = {}
-
-    user_restaurant = raw_input("Please enter a restaurant: ")
-    user_rating = raw_input("Please enter the rating: ")
+    file_ratings = {}
 
     with open(filename) as scores:
         for line in scores:
             restaurant, rating = line.strip().split(":")
-            ratings[restaurant] = rating
+            file_ratings[restaurant] = rating
 
-    ratings[user_restaurant] = user_rating
+    return file_ratings
+
+
+def get_user_ratings(file_ratings):
+    """Adds user restaurant and rating"""
+
+    user_restaurant = raw_input("Please enter a restaurant: ")
+    user_rating = raw_input("Please enter the rating: ")
+
+    file_ratings[user_restaurant] = user_rating
+
+
+def sort_ratings(restaurants):
+    """Sorts restaurant ratings by restaurant name."""
+
+    sorted_restaurants = sorted(restaurants.items())
+
+    for restaurant in sorted_restaurants:
+        print "{} is rated at {}.".format(restaurant[0], restaurant[1])
+
+
+def show_ratings(filename):
+    """Get file ratings, get usr rating, sort all ratings and print"""
+
+    ratings = get_file_ratings(filename)
+
+    get_user_ratings(ratings)
 
     sort_ratings(ratings)
 
-get_ratings("scores.txt")
+show_ratings("scores.txt")
